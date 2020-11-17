ab2b7c76f7c9aa0f4e55b4bb63ad2123699ed02c
shitikakei <shitikakei@gmail.com>
2020-11-18 02:26:20 +0900

    GroupList_Readonlyへの導線追加

--- frontend/src/todocomponents/TaskList_Readonly.js
+++ frontend/src/todocomponents/TaskList_Readonly.js
@@ -320,14 +320,23 @@ const Task_ReadOnly_List = () => {
           }}
         />
       </Pagination>
+      <div className="text-center">
+        <Button
+          variant="success"
+          className="mr-2"
+          onClick={() => history.goBack()}
+        >
+          Go Back Top
+        </Button>
 
-      <Button
-        variant="success"
-        className="mr-2"
-        onClick={() => history.goBack()}
-      >
-        Go Back Top
-      </Button>
+        <Button
+          variant="info"
+          className="mr-2"
+          onClick={() => history.push(`/user_group/list/${username}`)}
+        >
+          {username}`s Group
+        </Button>
+      </div>
     </div>
   );
 };
