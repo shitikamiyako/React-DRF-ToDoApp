20f39f190629dc897d6d68732d662a35fa18e335
shitikakei <shitikakei@gmail.com>
2020-11-18 23:12:37 +0900

    Reactのルーティングに対応

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    re_path(r'^$', views.index, name='index_page'),
+    re_path(r'^(?:.*)/?$', views.index, name="other_page"),
 ]
