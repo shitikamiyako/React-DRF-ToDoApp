aad91eac8d96e65493f09da42930d9122c1eae12
shitikakei <shitikakei@gmail.com>
2020-11-18 23:55:36 +0900

    urls.py修正

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,8 +1,29 @@
-from django.urls import path,re_path
+from django.urls import path,path
 from . import views
 
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index_page'),
-    re_path(r'^(?:.*)/?$', views.index, name="other_page"),
+    path('', views.index, name='index_page'),
+    path('login/', views.index, name='other_page'),
+    path('logout/', views.index, name='other_page'),
+    path('todo/top/', views.index, name='other_page'),
+    path('todo/list/<str>', views.index, name='other_page'),
+    path('todo/delete/<int:pk>', views.index, name='other_page'),
+    path('todo/edit/<int:pk>', views.index, name='other_page'),
+    path('todo/timer/<int:pk>', views.index, name='other_page'),
+    path('user_info/', views.index, name='other_page'),
+    path('password_change/', views.index, name='other_page'),
+    path('unsubscribe/', views.index, name='other_page'),
+    path('user_group/top/', views.index, name='other_page'),
+    path('user_group/joined/', views.index, name='other_page'),
+    path('user_group/list/<str>', views.index, name='other_page'),
+    # path(r'^login/$', views.index, name="other_page"),
+    # path(r'^logout/$', views.index, name="other_page"),
+    # path(r'^todo/top/$', views.index, name="other_page"),
+    # path(r'^todo/list/$', views.index, name="other_page"),
+    # path(r'^login/$', views.index, name="other_page"),
+    # path(r'^login/$', views.index, name="other_page"),
+    # path(r'^login/$', views.index, name="other_page"),
+    # path(r'^login/$', views.index, name="other_page"),
+    # path(r'^login/$', views.index, name="other_page"),
 ]
