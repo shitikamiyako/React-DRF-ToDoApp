013f8ebd6bd499f1afbfe2fc0cbd1519c7620bf9
shitikakei <shitikakei@gmail.com>
2020-11-20 01:56:17 +0900

    url変更

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,path
+from django.urls import re_path ,path
 from . import views
 
 
@@ -8,19 +8,26 @@ urlpatterns = [
     path('signup/', views.index, name='other_page'),
     path('logout/', views.index, name='other_page'),
     path('todo/top/', views.index, name='other_page'),
-    path('todo/list/<slug>/', views.index, name='other_page'),
-    path('todo/delete/<int:pk>/', views.index, name='other_page'),
-    path('todo/edit/<int:pk>/', views.index, name='other_page'),
-    path('todo/timer/<int:pk>/', views.index, name='other_page'),
+    path('todo/list/', views.index, name='other_page'),
+    re_path(r'^todo/list/[^/]+/$', views.index, name='other_page'),
+    re_path(r'^todo/delete/[0-9]+/$', views.index, name='other_page'),
+    re_path(r'^todo/edit/[0-9]+/$', views.index, name='other_page'),
+    re_path(r'^todo/timer/[0-9]+/$', views.index, name='other_page'),
+    # path('todo/edit/<int:number>/', views.index, name='other_page'),
+    # path('todo/timer/<int:number>/', views.index, name='other_page'),
+    # path('user_group/edit/<int:number>/', views.index, name='other_page'),
+    # path('user_group/delete/<int:number>/', views.index, name='other_page'),
+    # path('user_group/<int:pk>/members/', views.index, name='other_page'),
+    # path('user_group/list/<slug>/', views.index, name='other_page'),
     path('user_info/', views.index, name='other_page'),
     path('password_change/', views.index, name='other_page'),
     path('unsubscribe/', views.index, name='other_page'),
     path('user_group/top/', views.index, name='other_page'),
-    path('user_group/edit/<int:pk>/', views.index, name='other_page'),
-    path('user_group/delete/<int:pk>/', views.index, name='other_page'),
-    path('user_group/<int:pk>/members/', views.index, name='other_page'),
+    re_path(r'^user_group/edit/[0-9]+/$', views.index, name='other_page'),
+    re_path(r'^user_group/delete/[0-9]+/$', views.index, name='other_page'),
+    re_path(r'^user_group/[0-9]+/members/$', views.index, name='other_page'),
     path('user_group/joined/', views.index, name='other_page'),
-    path('user_group/list/<slug>/', views.index, name='other_page'),
+    re_path(r'^user_group/list/[^/]+/$', views.index, name='other_page'),
 ]
 
 handler500 = views.my_customized_server_error
